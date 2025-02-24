4e95500d047a136693a1669f648d4335bec65488
mahm0udismail <mahmoudismail9114@gmail.com>
2025-02-24 17:24:36 +0200

    add more than type to in test

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,7 +1,11 @@
 import pytest
 from tests.utils.client import APIClient
 from tests.utils.fixtures import test_client
-from tests.utils.mock_data import item_data, updated_item_data
+from tests.utils.mock_data import (item_data, 
+                                   updated_item_data,
+                                   invalid_item_data_missing_name,
+                                   invalid_item_data_empty_name
+                                   )
 
 @pytest.fixture
 def api_client(test_client):
@@ -11,6 +15,26 @@ def test_add_item(api_client):
     response = api_client.post("/items", item_data)
     assert response.status_code == 201
     assert response.json["item"]["name"] == item_data["name"]
+
+@pytest.mark.parametrize(
+    "payload, expected_status, expected_message",
+    [
+        (item_data, 201, "New Item"),  
+        (invalid_item_data_missing_name, 400, "Invalid data"),  
+        (None, 400, "Invalid data"),
+    ]
+)
+
+
+def test_add_item(api_client, payload, expected_status, expected_message):
+    response = api_client.post("/items", payload)
+    
+    assert response.status_code == expected_status
+    
+    if expected_status == 201:  # Success case
+        assert response.json["item"]["name"] == expected_message
+    else:  # Error case
+        assert response.json["error"] == expected_message
 
 def test_update_item(api_client):
     post_response = api_client.post("/items", item_data)
--- tests/utils/mock_data.py
+++ tests/utils/mock_data.py
@@ -1,2 +1,5 @@
-item_data = {"name": "Test Item"}
+item_data = {"name": "New Item"}
+
 updated_item_data = {"name": "Updated Item"}
+invalid_item_data_missing_name = {}  
+invalid_item_data_empty_name = {"name": ""} 
